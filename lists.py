a28154128718b56fbb572780fad16efd9afd4321
jackmarley254 <engndiritu1@gmail.com>
2023-10-11 08:00:11 +0300

    A program that converts miles to kilometers

--- lists.py
+++ lists.py
@@ -3,4 +3,4 @@ a = 'Good morning learners'
 print(a)
 b = 'Jack is the new programmer'
 print(b)
-print(b[1:5])
+print(b[0])
---
+++ pracrice.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+miles = input("Enter the number of miles you want to convert")
+
+miles = int(miles)
+kilometers = miles * 1.60934
+print("{}  ,miles is equals to {}  kilometers".format(miles, kilometers))
