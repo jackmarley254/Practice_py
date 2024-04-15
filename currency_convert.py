68ca095d301d027ebb23fbcb5d454ae7e5db50af
jackmarley254 <engndiritu1@gmail.com>
2024-04-15 12:01:33 +0300

    Currency converter

---
+++ currency_convert.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Use this exchange rate
+NAIRA_PER_DOLLAR = 410.59 # exchange rate as of Nov 10 2021
+
+USD_Value = float(input("Enter amount of USD to convert to NGN: "))
+Naira_Value = USD_Value * NAIRA_PER_DOLLAR
+print(f"{USD_Value} is {Naira_Value:.2f} NGN")
