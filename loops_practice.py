d433b7c052979030c245e9e936524fa165b3f05d
jackmarley254 <engndiritu1@gmail.com>
2024-04-18 16:30:27 +0300

    Loops practice

---
+++ loops_practice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+# Print numbers 1 to 100
+# For multiples of 3, print "X is a multiple of 3"
+# For multiples of 5, print "X is a multiple of 5"
+x = 0
+
+for x in range(1,101):
+  print(x)
+  if x % 3 == 0:
+    print(x, "is a multiple of 3")
+  elif x % 5 == 0:
+    print(x, "is a multiple of 5")
